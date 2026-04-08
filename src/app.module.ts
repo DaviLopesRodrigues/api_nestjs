7915c74d909403d5d34799573b19224ce0aa067e
Davi Lopes <davilopesrdgs@gmail.com>
2026-04-08 19:55:55 -0300

    3. module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
-//Decorators são funções que servem para modificar, manipular ou personalizar classes, funçães e etc..
-//São identificados pelo uso do @ (@Module) e por vir antes do alvo (nesse caso a classe AppModule)
-//O NestJS por padrão tem vários, mas é possível criar personalizados
-// Outro ponto interessante é que é possível adicionar vários decorators acima do alvo
+//O Module (app.module.ts) é um conceito do Nest que consistem em separar a aplicação em pequenos módulos
+//Resumidamente é como se fosse um agrupador de recursos (geralmente controllers e services)
+//É possivel importar módulos de terceiros (imports: []) e exportar módulos para a outros módulos da aplicação (exports: [])
 @Module({
   imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
-//Obs -> Os decorators são utilizados em toda arquitetura do Nest (services, controllers, modules, etc)
