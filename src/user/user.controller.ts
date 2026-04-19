5811f8f50008a27f9281b72b980a29863fbfd15d
davilopes <davilopesrdgs@gmail.com>
2026-04-18 22:25:32 -0300

    7. read (crud)

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,14 +1,35 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
 
 //Controller users
 
-@Controller("users")
+@Controller('users')
 export class UserController {
+  //Post - Criação de usuário no banco
+  @Post()
+  //Body -> Recuperação do corpo da requisição (vindo em JSON)
+  create(@Body() body) {
+    return { body };
+  }
 
-    //Post - Criação de recurso no banco
-    @Post()
-    //Body -> Recuperação do corpo da requisição (vindo em JSON)
-    createUser(@Body() body)  {
-        return {body}
-     }
+  //Get - Listagem de usuários do banco
+  @Get()
+  findAll(){
+    return {users: []}
+  }
+
+  //Get - Listagem de usuário do banco
+  // Obs -> O '/:id' que será necessário informar na requisição é o id do usuário pesquisado 
+  @Get('/:id')
+  //Param -> Recuperação do paramâmetro enviado na URL da requisição (identificar recurso específico)
+  findOne(@Param("id") param){
+    return {user: {}, param}
+  }
+
+  //Get - Listagem de usuários do banco
+  @Get()
+  //Query -> Recuperação da query enviada na URL da requisição (filtro, paginação, ordenação)
+  findStatus(@Query("status") status){
+    return {users: []}
+  }
+  
 }
