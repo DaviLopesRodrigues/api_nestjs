90d42872bf5f516e176d30620b66c3a343950bef
davilopes <davilopesrdgs@gmail.com>
2026-04-19 22:30:21 -0300

    8. update (crud)

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,13 @@
-import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
 
 //Controller users
 
@@ -13,23 +22,40 @@ export class UserController {
 
   //Get - Listagem de usuários do banco
   @Get()
-  findAll(){
-    return {users: []}
+  findAll() {
+    return { users: [] };
   }
 
   //Get - Listagem de usuário do banco
-  // Obs -> O '/:id' que será necessário informar na requisição é o id do usuário pesquisado 
-  @Get('/:id')
+  // Obs -> O '/:id' que será necessário informar na requisição é o id do usuário pesquisado
+  @Get(':id')
   //Param -> Recuperação do paramâmetro enviado na URL da requisição (identificar recurso específico)
-  findOne(@Param("id") param){
-    return {user: {}, param}
+  findOne(@Param('id') param) {
+    return { user: {}, param };
   }
 
   //Get - Listagem de usuários do banco
   @Get()
   //Query -> Recuperação da query enviada na URL da requisição (filtro, paginação, ordenação)
-  findStatus(@Query("status") status){
-    return {users: []}
+  findStatus(@Query('status') status) {
+    return { users: [] };
   }
-  
+
+  //Put -> Atualização de usuário no banco
+  // Obs -> O '/:id' que será necessário informar na requisição é o id do usuário pesquisado
+  @Put(':id')
+  //Body -> Recuperação do corpo da requisição (vindo em JSON)
+  //Param -> Recuperação do paramâmetro enviado na URL da requisição (identificar recurso específico)
+  update(@Body() body, @Param('id') param) {
+    return { user: {} };
+  }
+
+  //Patch -> Atualização parcial de usuário no banco
+  // Obs -> O '/:id' que será necessário informar na requisição é o id do usuário pesquisado
+  @Patch(':id')
+  //Body -> Recuperação do corpo da requisição (vindo em JSON)
+  //Param -> Recuperação do paramâmetro enviado na URL da requisição (identificar recurso específico)
+  updatePartial(@Body() body, @Param('id') param) {
+    return { user: {} };
+  }
 }
