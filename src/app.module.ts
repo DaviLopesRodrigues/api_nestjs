09fe728d7049e21d6cd8c6d094e3a36906bb7e15
davilopes <davilopesrdgs@gmail.com>
2026-06-22 22:34:13 -0300

    25: protecao contra rate limiting (throttler)

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,19 @@ import { LogModule } from './log/log.module';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { CryptoModule } from './crypto/crypto.module';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
   imports: [
+    ThrottlerModule.forRoot({
+      throttlers: [
+        //Essa configuração faz com que a API só receba 10 requisições por endpoint a cada 1 minuto (depois disso o servidor emite uma exception)
+        {
+          ttl: 60000,
+          limit: 10,
+        },
+      ],
+    }),
     ConfigModule.forRoot({ isGlobal: true }),
     PrismaModule,
     UserModule,
@@ -16,6 +26,12 @@ import { CryptoModule } from './crypto/crypto.module';
     CryptoModule,
   ],
   controllers: [],
-  providers: [],
+  //Definindo o Throttler de forma global na API (mas é possível adicionar de forma local, por cima do controller todo ou endpoint específico)
+  providers: [
+    {
+      provide: 'APP_GUARD',
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
