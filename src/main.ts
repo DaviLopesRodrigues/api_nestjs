6df35fc11a782773365060fc04b7c09ef7905ee0
davilopes <davilopesrdgs@gmail.com>
2026-06-22 21:55:44 -0300

    24. cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,14 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: ['http://meusite.com.br'], //Origens que podem fazer requisições
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'], //Métodos que o navegador permite executar
+    allowHeaders: ['Content-Type', 'Authorization'], //Cabeçalhos permitidos na requisição
+    credentials: true, //Permissão de envio de cookies e certificados na requisição
+    maxAge: 86400, //Tempo que o navegador guarda em cache a resposta do preflight
+  });
+
   app.useGlobalPipes(new ValidationPipe());
 
   //Fechar conexão com o Banco de Dados após realizar operações
